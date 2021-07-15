2a6f32ff6def70cd648ae78d0a39bdaae70dd1ac
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-15 06:38:30 +0300

    fixed django superuser script

--- bcsproject/entrypoint.sh
+++ bcsproject/entrypoint.sh
@@ -12,7 +12,8 @@ fi
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-echo "from django.contrib.auth.models import User;
+echo "from django.contrib.auth import get_user_model;
+User = get_user_model()
 User.objects.filter(email='$DJANGO_ADMIN_EMAIL').delete();
 User.objects.create_superuser('$DJANGO_ADMIN_FNAME', '$DJANGO_ADMIN_LNAME', '$DJANGO_ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')" | python manage.py shell
 
